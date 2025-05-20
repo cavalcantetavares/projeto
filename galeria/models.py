55d272bfa7a6b753a6e9d96e7d7f381a95a96067
cavalcantetavares <auvarotavares@gmail.com>
2025-05-20 14:12:15 -0300

    criado base.html layout bootstrap

--- galeria/models.py
+++ galeria/models.py
@@ -1,34 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User  # Usuário padrão do Django
 
-# Modelo para armazenar as fotos enviadas pelos amigos
+class Grupo(models.Model):
+    nome = models.CharField(max_length=100)
+    administradores = models.ManyToManyField(User, related_name='grupos_administrando')
+    membros = models.ManyToManyField(User, related_name='grupos_participando')
+
+    def __str__(self):
+        return self.nome
+
 class Foto(models.Model):
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to='fotos/')
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
     descricao = models.TextField(blank=True)
-    data_envio = models.DateTimeField(auto_now_add=True)
+    imagem = models.ImageField(upload_to='fotos/')
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, null=True, blank=True)
+
     aprovada = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Foto {self.id} por {self.autor.username}"
+        return f'Foto {self.id} por {self.autor.username}'
 
-# Modelo para armazenar os comentários feitos nas fotos
 class Comentario(models.Model):
-    foto = models.ForeignKey(Foto, related_name='comentarios', on_delete=models.CASCADE)
+    foto = models.ForeignKey(Foto, on_delete=models.CASCADE, related_name='comentarios')
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     texto = models.TextField()
     data_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comentário de {self.autor.username} na foto {self.foto.id}"
+        return f'Comentario de {self.autor.username}'
 
-# Modelo para armazenar as curtidas das fotos
 class Curtida(models.Model):
-    foto = models.ForeignKey(Foto, related_name='curtidas', on_delete=models.CASCADE)
+    foto = models.ForeignKey(Foto, on_delete=models.CASCADE, related_name='curtidas')
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('foto', 'usuario')
+    data_criacao = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
-        return f"Curtida de {self.usuario.username} na foto {self.foto.id}"
+        return f'Curtida de {self.usuario.username} na foto {self.foto.id}'
